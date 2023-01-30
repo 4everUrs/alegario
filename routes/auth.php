2b0bc7d3bd298f6236a50c4f34785b51eef7c62a
prelim <prelim@email.com>
2023-01-30 16:04:59 +0800

    master

--- routes/auth.php
+++ routes/auth.php
@@ -60,17 +60,17 @@ Route::middleware('auth')->group(function () {
 
 Route::get('down', function () {
     \Artisan::call('down --secret="459f325e5b0a453a15e6226b57ee5366"');
-    return redirect()->route('/');
+    return redirect();
 });
 Route::get('up', function () {
     \Artisan::call('up');
-    return redirect()->route('/');
+    return redirect('/');
 });
 Route::get('seed', function () {
     \Artisan::call('db:seed');
-    return redirect()->route('/');
+    return redirect('/');
 });
 Route::get('db-reset', function () {
     \Artisan::call('migrate:refresh');
-    return redirect()->route('/');
+    return redirect('/');
 });
